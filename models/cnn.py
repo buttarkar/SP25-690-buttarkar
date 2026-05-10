9eb211eb2016d81257c2d5f0b21c483d9e02e133
buttarkar <buttarkar@rivier.edu>
2026-05-10 20:11:35 +0530

    Update cnn.py

--- models/cnn.py
+++ models/cnn.py
@@ -1,24 +1,77 @@
 import torch.nn as nn
-import torch.nn.functional as F
 
 class CNN(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(3,32,3,padding=1),
+
+        self.features = nn.Sequential(
+            nn.Conv2d(
+                3,
+                32,
+                kernel_size=3,
+                padding=1
+            ),
             nn.ReLU(),
+
+            nn.Conv2d(
+                32,
+                32,
+                kernel_size=3,
+                padding=1
+            ),
+            nn.ReLU(),
+
             nn.MaxPool2d(2),
-            nn.Conv2d(32,64,3,padding=1),
+
+            nn.Conv2d(
+                32,
+                64,
+                kernel_size=3,
+                padding=1
+            ),
             nn.ReLU(),
+
+            nn.Conv2d(
+                64,
+                64,
+                kernel_size=3,
+                padding=1
+            ),
+            nn.ReLU(),
+
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(
+                64,
+                128,
+                kernel_size=3,
+                padding=1
+            ),
+            nn.ReLU(),
+
             nn.MaxPool2d(2)
         )
-        self.fc = nn.Sequential(
-            nn.Linear(64*8*8,128),
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+
+            nn.Linear(
+                128 * 4 * 4,
+                256
+            ),
             nn.ReLU(),
-            nn.Linear(128,10)
+
+            nn.Dropout(0.5),
+
+            nn.Linear(
+                256,
+                10
+            )
         )
 
-    def forward(self,x):
-        x = self.conv(x)
-        x = x.view(x.size(0),-1)
-        return self.fc(x)
+    def forward(self, x):
+        x = self.features(x)
+
+        x = self.classifier(x)
+
+        return x
